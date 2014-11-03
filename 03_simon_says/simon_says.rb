d9e834ce0b28169c90432427d7a525bc5d665eb7
Rudi Luzar <rudi@rudicode.com>
2014-11-03 13:20:44 -0500

    Completed 03_simon_saya

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -14,6 +14,24 @@ def repeat word, num = 2
   result
 end
 
-def start_of_word
-  
+def start_of_word word, number_of_letters
+  word[0..(number_of_letters-1)]
+end
+
+def first_word sentence
+  word = sentence.split.first
+end
+
+def titleize sentence
+  skip_words = ["and","over","the"]
+  words = sentence.split
+  words.map! do |word|
+    if skip_words.include?(word)
+      word
+    else
+      word.capitalize!
+    end
+  end
+  words[0].capitalize!
+  words.join(' ')  
 end
